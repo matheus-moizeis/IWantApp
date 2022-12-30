2d031fb585b0e0ec785f59851f8f6e099989f537
Matheus Moizeis <matheusmoizeis@outlook.com>
2022-12-30 16:45:14 -0300

    instalando o identity e configurando service.

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,14 @@
 using IWantApp.EndPoints.Categories;
 using IWantApp.Infra.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionStrings:IWantDb"]);
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
