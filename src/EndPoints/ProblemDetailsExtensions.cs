b46196155fd2b7d125c81428fdb64e3c1465cc9c
Matheus Moizeis <matheusmoizeis@outlook.com>
2022-12-31 15:20:30 -0300

    refaturando mensagem de erro ao adicionar usuario

--- src/EndPoints/ProblemDetailsExtensions.cs
+++ src/EndPoints/ProblemDetailsExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Flunt.Notifications;
+using Microsoft.AspNetCore.Identity;
 
 namespace IWantApp.EndPoints;
 
@@ -10,4 +11,13 @@ public static class ProblemDetailsExtensions
             .GroupBy(g => g.Key)
             .ToDictionary(g => g.Key, g => g.Select(x => x.Message).ToArray());
     }
+
+    public static Dictionary<string, string[]> ConvertToProblemDetails(this IEnumerable<IdentityError> error)
+    {
+        var dictionary = new Dictionary<string, string[]>();
+        dictionary.Add("Error", error.Select(e => e.Description).ToArray());
+        
+        return dictionary;
+    }
+
 }
