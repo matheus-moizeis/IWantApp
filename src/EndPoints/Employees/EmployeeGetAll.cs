1bc1ab26b258d598d64797263f21485756564373
Matheus Moizeis <matheusmoizeis@outlook.com>
2022-12-31 16:08:10 -0300

    adicionado GetAll de usuarios com paginacao

---
+++ src/EndPoints/Employees/EmployeeGetAll.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace IWantApp.EndPoints.Employees;
+
+public class EmployeeGetAll
+{
+    public static string Template => "/employee";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(int page, int rows, UserManager<IdentityUser> userManager)
+    {
+        var users = userManager.Users.Skip((page - 1 ) * rows).Take(rows).ToList();
+
+        var employees = users.Select(u => new EmployeeResponse( u.Email, "Teste" ));
+
+        return Results.Ok(employees);
+    }
+}
---
+++ src/EndPoints/Employees/EmployeeResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace IWantApp.EndPoints.Employees;
+
+public record EmployeeResponse(string Email, string Name);
