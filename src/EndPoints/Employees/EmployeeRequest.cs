d0cc004e8874a39cf2ae4fb8bda8ba84cc941324
Matheus Moizeis <matheusmoizeis@outlook.com>
2022-12-31 01:09:02 -0300

    adicionado metodo post para usuario

---
+++ src/EndPoints/Employees/EmployeeRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace IWantApp.EndPoints.Employees;
+
+public record EmployeeRequest(string Email, string Password, string Name);
--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,17 @@
 using IWantApp.EndPoints.Categories;
+using IWantApp.EndPoints.Employees;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionStrings:IWantDb"]);
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddSqlServer<ApplicationDbContext>(
+    builder.Configuration["ConnectionStrings:IWantDb"]);
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+}).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,5 +30,6 @@ app.UseHttpsRedirection();
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
 
 app.Run();
